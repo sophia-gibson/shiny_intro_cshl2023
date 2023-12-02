37a71783b72d01dd04a14a0678c3990a2c5837c5
Sophia Gibson <sbgibson98@gmail.com>
2023-12-01 22:13:08 -0500

    added description

--- app.R
+++ app.R
@@ -1,11 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is a modified default shiny app using the old faithful geyser data. It was made for the 2023 Cold Spring Harbor Laboratory Computational Genomics Workshop.
 
 library(shiny)
 library(ggplot2)

00989e04cc3a47922fa438a5aca18ac5641a4a5f
Sophia Gibson <sbgibson98@gmail.com>
2023-12-01 21:14:04 -0500

    added app.R

---
+++ app.R
@@ -0,0 +1,58 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(bslib)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(theme = bs_theme(version = 4, bootswatch = "minty"),
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+      
+      #ggplot instead of base R
+      ggplot(data=faithful, aes(x=waiting, stat="identity")) +
+        geom_histogram(bins = input$bins, fill="darkgrey") +
+        xlab("Waiting time to next eruption (min)") +
+        ylab("Frequency") +
+        theme(axis.text = element_text(size=18),
+              axis.title = element_text(size=18),
+              panel.background = element_rect(fill = "white"),
+              panel.border = element_rect(colour = "black", fill=NA),
+              panel.grid.major = element_line(size = 0.25, linetype = 'solid',
+                                              colour = "grey"))
+
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
